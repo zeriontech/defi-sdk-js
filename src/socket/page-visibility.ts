80a11ccc126671eb2594fb2ba2f11163f4bae0b3
everdimension <everdimension@gmail.com>
2022-11-30 13:04:22 +0700

    Handle non DOM envs: replace window with globalThis

--- src/socket/page-visibility.ts
+++ src/socket/page-visibility.ts
@@ -1,3 +1,4 @@
+/* global globalThis */
 import type { Socket } from "socket.io-client";
 import type { Unsubscribe } from "../shared/Unsubscribe";
 
@@ -6,7 +7,7 @@ const WAIT_BEFORE_DISCONNECT = 20000;
 function getWaitTime() {
   // just a helper for debugging so that timeout
   // can be set from console
-  return (window as any).WAIT_BEFORE_DISCONNECT || WAIT_BEFORE_DISCONNECT;
+  return (globalThis as any).WAIT_BEFORE_DISCONNECT || WAIT_BEFORE_DISCONNECT;
 }
 
 export function handlePageVisibility(socket: typeof Socket): Unsubscribe {
