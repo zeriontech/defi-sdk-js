b8321aac00f2304c013d3c398833cb65cbf12785
Andrey <zertsalov96@yandex.ru>
2023-03-29 13:41:29 +0100

    add context to portfolio decomposition payload (#47)

--- src/domains/addressPortfolioDecomposition.ts
+++ src/domains/addressPortfolioDecomposition.ts
@@ -2,9 +2,18 @@ import { PortfolioDecomposition } from "../entities";
 import { AddressParams } from "./AddressParams";
 import { createDomainRequest } from "./createDomainRequest";
 
+type PortfolioDecompositionContext =
+  | "watchlist"
+  | "my_wallets"
+  | "manage_wallets"
+  | "background_request";
+
 export type Payload = AddressParams & {
   currency: string;
   nft_price_type?: "floor_price" | "buy_now_price" | "not_included";
+  meta?: {
+    context?: PortfolioDecompositionContext;
+  };
 };
 
 export const namespace = "address";
